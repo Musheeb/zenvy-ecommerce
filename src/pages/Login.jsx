4cc01b85290e142db383c260d2c1f5649b75507c
Musheeb <museebnoorisys240@gmail.com>
2026-04-22 23:37:01 +0530

    feat: moving forward gradually with login form UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,16 +4,51 @@ export default function Login() {
   return (
     <div className={styles.container}>
       <div className={styles.branding}>
-        <img
-          className={styles.zenvyImage}
-          src="ZenvyCartLogo.png"
-          alt="Zenvy Cart Logo"
-        />
-        <span>ONE CART. ENDLESS CHOICES.</span>
+        <span className={styles.zenvyBranding}>ZENVY</span>
+        <span className={`${styles.texts}`}>ONE CART. ENDLESS CHOICES.</span>
       </div>
-      <div className={styles.loginBox}></div>
+      <div className={styles.loginBox}>
+        <div className={styles.loginContents}>
+          <span className={styles.signInText}>Sign In</span>
+          <p className={styles.signInQuoteText}>
+            Access your curated collection and order history.
+          </p>
+          <label className={styles.labels} htmlFor="email">
+            EMAIL ADDRESS
+          </label>
+          <input
+            className={`${styles.inputs}`}
+            type="email"
+            id="email"
+            name="email"
+            placeholder="curator@zenvy.com"
+          />
+          <label className={styles.labels} htmlFor="password">
+            PASSWORD
+          </label>
+          <input
+            className={`${styles.inputs}`}
+            type="password"
+            id="password"
+            placeholder="*********"
+          />
+          <button className={`${styles.buttons}`}>LOGIN</button>
+          <p className={styles.loginOptionSeparatorText}>OR CONTINUE WITH</p>
+          <button className={`${styles.buttons}`}>LOGIN WITH GOOGLE</button>
+          <p>
+            Don't have an account? <span>Sign Up</span>
+          </p>
+        </div>
+      </div>
       <div className={styles.logoAndQuote}>
-        <span>"Everything you need, just one login away."</span>
+        <img
+          className={styles.logoSober}
+          src="ZenvyLogoSober1.png"
+          alt="Zenvy Logo Sober"
+        />
+        <p className={`${styles.texts} ${styles.quote}`}>
+          Everything you need, just one login away.
+        </p>
       </div>
     </div>
   );
