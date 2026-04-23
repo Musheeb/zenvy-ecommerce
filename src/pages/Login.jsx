ea7b0c2bd5f0fb36e29d91486aad711de7cebeba
Musheeb <museebnoorisys240@gmail.com>
2026-04-23 23:54:33 +0530

    feat: login UI completed with show password state management

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,13 @@
 import styles from "../styles/Login.module.css";
 
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  function handleShowPassword() {
+    setShowPassword((prev) => !prev);
+  }
   return (
     <div className={styles.container}>
       <div className={styles.branding}>
@@ -23,20 +30,50 @@ export default function Login() {
             name="email"
             placeholder="curator@zenvy.com"
           />
-          <label className={styles.labels} htmlFor="password">
-            PASSWORD
-          </label>
-          <input
-            className={`${styles.inputs}`}
-            type="password"
-            id="password"
-            placeholder="*********"
-          />
-          <button className={`${styles.buttons}`}>LOGIN</button>
+          <div className={styles.inputGroup}>
+            <div className={styles.passwordLabelForgetPasswordWrapper}>
+              <label className={styles.labels} htmlFor="password">
+                PASSWORD
+              </label>
+              <Link className={styles.forgetPassword}>FORGOT PASSWORD?</Link>
+            </div>
+            <div className={styles.passwordInputWrapper}>
+              <input
+                className={`${styles.inputs}`}
+                type={showPassword ? "text" : "password"}
+                id="password"
+                placeholder="*********"
+              />
+              {showPassword ? (
+                <span
+                  className={`material-symbols-outlined ${styles.eyeIcon}`}
+                  onClick={handleShowPassword}
+                >
+                  visibility_off
+                </span>
+              ) : (
+                <span
+                  className={`material-symbols-outlined ${styles.eyeIcon}`}
+                  onClick={handleShowPassword}
+                >
+                  visibility
+                </span>
+              )}
+            </div>
+          </div>
+          <button className={`${styles.buttons} ${styles.loginButton}`}>
+            LOGIN
+          </button>
           <p className={styles.loginOptionSeparatorText}>OR CONTINUE WITH</p>
-          <button className={`${styles.buttons}`}>LOGIN WITH GOOGLE</button>
+          <button
+            className={`${styles.buttons} ${styles.loginWithGoogleButton}`}
+          >
+            <i className={`fa-brands fa-google ${styles.googleLogo}`}></i>
+            LOGIN WITH GOOGLE
+          </button>
           <p>
-            Don't have an account? <span>Sign Up</span>
+            Don't have an account?{" "}
+            <Link className={styles.signUpLink}>Sign Up</Link>
           </p>
         </div>
       </div>
