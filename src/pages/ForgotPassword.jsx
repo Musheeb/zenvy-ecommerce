b8da257af689dedc87675162c654748b44f5f5d3
Musheeb <museebnoorisys240@gmail.com>
2026-04-28 00:08:26 +0530

    feat: reset passwork link sent confirmation popup UI

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -2,8 +2,16 @@ import { Link } from "react-router-dom";
 import styles from "../styles/ForgotPassword.module.css";
 
 import { ROUTES } from "../routes/routes";
+import { useState } from "react";
 
 export default function ForgotPassword() {
+  const [sendResetLink, setSendResetLink] = useState(false);
+  function handleSendResetLink() {
+    setSendResetLink(true);
+  }
+  function handleClosePopup() {
+    setSendResetLink(false);
+  }
   return (
     // <div className={styles.page}>
     <div className={styles.container}>
@@ -23,13 +31,36 @@ export default function ForgotPassword() {
           id="email"
           className={styles.emailInput}
         />
-        <button className={styles.sendLinkButton}>SEND RESET LINK</button>
+        <button className={styles.sendLinkButton} onClick={handleSendResetLink}>
+          SEND RESET LINK
+        </button>
         <hr className={styles.horizontalRuler} />
         <Link to={ROUTES.LOGIN} className={styles.returnToLoginWrapper}>
           <span className="material-symbols-outlined">arrow_back</span>
           <span className={styles.returnToLoginText}>RETURN TO LOGIN</span>
         </Link>
       </div>
+      {sendResetLink && (
+        <div className={styles.sendResetPasswordLinkPopupContainer}>
+          <div className={styles.passworResetLinkSentInformPopup}>
+            <span className={styles.resetLinkPopupText}>
+              Reset password link has been sent to your associated email address
+              successfully.
+            </span>
+            <span
+              className={`material-symbols-outlined ${styles.tickIcon} ${styles.animateTick}`}
+            >
+              check_circle
+            </span>
+            <span
+              className={`material-symbols-outlined ${styles.closePopup}`}
+              onClick={handleClosePopup}
+            >
+              close
+            </span>
+          </div>
+        </div>
+      )}
     </div>
     // </div>
   );
